0ebdecf1ccb6a7c5476a44c23d7d704241fd83d8
0xluckycoder <lakshanperera.dev@gmail.com>
2025-11-08 22:49:38 +0530

    add ink

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ open-rabbit
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ open-rabbit --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
